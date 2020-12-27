0941c09bd14aa5d357d6738d08460331cfa16339
Mekyle <mekylenaidoo@yahoo.com>
2020-12-27 17:09:28 +0200

    Update

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,24 @@
-<script>
-	import Tailwindcss from './Tailwindcss.svelte';
+<link href="https://fonts.googleapis.com/css?family=Abhaya+Libre:400,800|Montserrat:600|Alegreya+Sans:500&display=swap" rel="stylesheet">
 
-	export let name;
-</script>
-
-<Tailwindcss />
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="font-abhaya-libre relative h-full flex items-center bg-gradient-to-r from-white to-green-300">
+	
+    <header class="h-24 sm:h-32 flex items-center absolute top-0 right-0 z-20">
+        <div class="container mx-auto px-6 sm:px-12 flex items-center justify-end">
+            <nav class="flex items-center">
+                <button href="#" class="block font-abhaya-libre uppercase text-pink-500 tracking-wider px-4 py-2 text-lg hover:underline">Telegram</button>
+                <button href="https://discord.gg/znrEDDQca8" class="hidden xl:block font-abhaya-libre uppercase text-gray-800 tracking-wider px-4 xl:px-8 py-2 text-lg hover:underline">Discord</button>
+                <button href="#" class="hidden xl:block font-abhaya-libre uppercase text-gray-800 tracking-wider px-4 xl:px-8 py-2 text-lg hover:underline">Twitter</button>
+            </nav>
+        </div>
+    </header>
+    <div class="container mx-auto px-6 sm:px-12 flex flex-col sm:flex-row items-center relative z-10">
+        <div class="sm:w-1/2 xl:w-2/6 flex flex-col items-start py-24 sm:py-0">
+            <h1 class="text-6xl xl:text-10xl font-abhaya-libre text-red-500 font-bold leading-none">EthGrounds</h1>
+            <h2 class="text-xl xl:text-3xl font-abhaya-libre text-yellow-500 uppercase font-bold leading-none tracking-widest ml-1 mb-6">Next-Gen Fundraising</h2>
+            <p class="xl:text-lg tracking-wider text-orange-500 font-alegraya-sans">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh  minim veniam, quis nostrud</p>
+            <button href="#" class="font-montserrat text-white sm:font-xl uppercase py-3 px-6 sm:py-4 sm:px-8 rounded-full shadow-lg bg-green-500 hover:bg-green-400 mt-8 sm:mt-16">Read more</button>
+        </div>
+        <div class="hidden sm:flex items-center justify-center xl:justify-end w-1/2 xl:w-4/6 py-32">
+        </div>
+    </div>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
